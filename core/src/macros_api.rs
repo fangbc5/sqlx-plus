950364fc22607d6142076e92aff17b23cf4fa263
fangbaichun <fangbaichun@beemwork.com>
2025-12-26 15:18:54 +0800

    优化警告

--- core/src/macros_api.rs
+++ core/src/macros_api.rs
@@ -1,9 +1,7 @@
 //! 宏接口模块，供 proc-macro crate 使用
 //!
 //! 这个模块提供了 derive 宏生成代码时需要的辅助函数和类型
-
-use crate::db_pool::{DbPool, Result};
-
+//! 
 /// 字段元数据，由 derive(ModelMeta) 生成
 pub struct FieldMeta {
     pub name: &'static str,
